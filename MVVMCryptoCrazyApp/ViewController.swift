6a7f335d7def2d7c37849768b51bfedf614e0d7e
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-05 13:14:33 +0300

    .

--- MVVMCryptoCrazyApp/ViewController.swift
+++ MVVMCryptoCrazyApp/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        
+
     }
 
 
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CryptoTableViewCell
+        
+        
+        return cell
+    }
+}
