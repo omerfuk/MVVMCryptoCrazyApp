7ef4a455ca65eab462a9ebdca398c9812db6852c
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-05 13:17:32 +0300

    Model File Created, Folderized

---
+++ MVVMCryptoCrazyApp/Model/CryptoCurrency.swift
@@ -0,0 +1,16 @@
+//
+//  CryptoCurrency.swift
+//  MVVMCryptoCrazyApp
+//
+//  Created by Ömer Faruk Kılıçaslan on 5.07.2022.
+//
+
+import Foundation
+
+
+struct CryptoCurrency : Codable  { // get and post -> CODABLE
+    
+    let currency : String
+    let price: String
+    
+}
