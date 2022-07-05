60b00634f04ec39128c127c42d1e4f5513eebf37
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-05 13:39:37 +0300

    ViewModel Files created

---
+++ MVVMCryptoCrazyApp/ViewModel/CryptoViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  CryptoViewModel.swift
+//  MVVMCryptoCrazyApp
+//
+//  Created by Ömer Faruk Kılıçaslan on 5.07.2022.
+//
+
+import Foundation
+
+struct CryptoListViewModel {
+    
+    let cryptoCurrencyList: [CryptoCurrency]
+    
+    func numberOfRowsInSection() -> Int {
+        return self.cryptoCurrencyList.count
+    }
+    
+    func cryptoAtIndex(_ index: Int) -> CryptoViewModel {
+        let crypto = self.cryptoCurrencyList[index]
+        return CryptoViewModel(cryptoCurrency: crypto)
+    }
+    
+}
+
+struct CryptoViewModel {
+    
+    let cryptoCurrency: CryptoCurrency
+    
+    var name : String {
+        return self.cryptoCurrency.currency
+    }
+    
+    var price: String {
+        return self.cryptoCurrency.price
+    }
+}
